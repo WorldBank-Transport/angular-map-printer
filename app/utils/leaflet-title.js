c6c3e4d800c8b1d052ee39c57dfc13ee59964585
Emil <bmachaku@gmail.com>
2016-02-11 17:42:30 +0300

    bug fix map title

--- app/utils/leaflet-title.js
+++ app/utils/leaflet-title.js
@@ -1,89 +1,45 @@
 /*
  * L.Control.Title is used for displaying title on the map (added by default).
- * Based on L.Control.Attribution
  */
 
 L.Control.Title = L.Control.extend({
 	options: {
 		position: 'topleft',
-		prefix: ''
+        id: '_leaflet-control-title'
 	},
 
 	initialize: function (options) {
 		L.setOptions(this, options);
 
-		this._titles = {};
+		this.title = '';
 	},
 
 	onAdd: function (map) {
 		this._container = L.DomUtil.create('div', 'leaflet-control-title');
+        this._container.id = this.options.id
 		if (L.DomEvent) {
 			L.DomEvent.disableClickPropagation(this._container);
 		}
-
-		// TODO ugly, refactor
-		for (var i in map._layers) {
-			if (map._layers[i].getTitle) {
-				this.addTitle(map._layers[i].getTitle());
-			}
-		}
-
 		this._update();
-
 		return this._container;
 	},
 
-	setPrefix: function (prefix) {
-		this.options.prefix = prefix;
-		this._update();
-		return this;
-	},
 
 	addTitle: function (text) {
 		if (!text) { return this; }
-
-		if (!this._titles[text]) {
-			this._titles[text] = 0;
-		}
-		this._titles[text]++;
-
-		this._update();
-
+        this._title = text
 		return this;
 	},
 
 	removeTitle: function (text) {
 		if (!text) { return this; }
-
-		if (this._titles[text]) {
-			this._titles[text]--;
-			this._update();
-		}
-
+        this._title = ''
 		return this;
 	},
 
 	_update: function () {
 		if (!this._map) { return; }
-
-		var attribs = [];
-
-		for (var i in this._titles) {
-			if (this._titles[i]) {
-				attribs.push(i);
-			}
-		}
-
-		var prefixAndAttribs = [];
-
-		if (this.options.prefix) {
-			prefixAndAttribs.push(this.options.prefix);
-		}
-		if (attribs.length) {
-			prefixAndAttribs.push(attribs.join(', '));
-		}
-
-		this._container.innerHTML = prefixAndAttribs.join(' | ');
+		this._container.innerHTML = this._title;
 	}
 });
 
